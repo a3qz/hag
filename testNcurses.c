55511a83e35182f317f29a0ae6ecd675fc2e05b8
phale01 <phale01@saintmarys.edu>
2018-02-13 11:05:35 -0500

    eddited the print function so that it can print anywhere easily

--- testNcurses.c
+++ testNcurses.c
@@ -8,7 +8,7 @@
 
 void init_wins(WINDOW **wins);
 void win_show(WINDOW *win, char *label, int label_color);
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
+void print_in_window(WINDOW *win, int starty, int startx, int width, char *string, chtype color, bool mid);
 
 int main()
 {
@@ -114,11 +114,11 @@ void win_show(WINDOW *win, char *label, int label_color)
 	mvwhline(win, 2, 1, ACS_HLINE, width - 2); 
 	mvwaddch(win, 2, width - 1, ACS_RTEE); 
 	
-	print_in_middle(win, 1, 0, width, label, COLOR_PAIR(label_color));
-	print_in_middle(win, 6, 0, width, "HELLO", COLOR_PAIR(label_color));
+	print_in_window(win, 1, 0, width, label, COLOR_PAIR(label_color), true);
+	print_in_window(win, 6, 1, width, "HELLO", COLOR_PAIR(label_color), false);
 }
 
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color)
+void print_in_window(WINDOW *win, int starty, int startx, int width, char *string, chtype color, bool mid)
 {	int length, x, y;
 	float temp;
 
@@ -133,7 +133,11 @@ void print_in_middle(WINDOW *win, int starty, int startx, int width, char *strin
 		width = 80;
 
 	length = strlen(string);
-	temp = (width - length)/ 2;
+	if (mid) {
+		temp = (width - length)/ 2;
+	} else {
+		temp = 0;
+	}
 	x = startx + (int)temp;
 	wattron(win, color);
 	mvwprintw(win, y, x, "%s", string);
