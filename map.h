a18d823c45633bdb7926ec34fc98a2148a881aa8
John Joseph Westhoff <jwesthof@nd.edu>
2018-02-27 12:52:51 -0500

    Changed char** to map_t

--- map.h
+++ map.h
@@ -2,7 +2,9 @@
 #define MAP_H
     #include <ncurses.h>
 
-    void map_load(char **new_map, int w, int h);
+    typedef char** map_t;
+
+    void map_load(map_t new_map, int w, int h);
     void map_set(char c, int y, int x);
     char map_get(int y, int x);
     void map_print(WINDOW *win, int y, int x);
