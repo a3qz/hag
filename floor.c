af920ab8f0c68a626f607269b371cfcd7eb4e44e
John Joseph Westhoff <jwesthof@nd.edu>
2018-02-27 13:09:14 -0500

    Floor after floor

---
+++ floor.c
@@ -0,0 +1,38 @@
+#include <ncurses.h>
+#include "floor.h"
+#include "list.h"
+
+static const int NUM_ROOMS = 30;
+static const int MAX_ROOM_X = 30;
+static const int MIN_ROOM_X = 3;
+static const int MAX_ROOM_Y = 30;
+static const int MIN_ROOM_Y = 3;
+static const int BOARD_X = 100;
+static const int BOARD_Y = 200;
+
+static floor_t floors[FLOOR_COUNT] = {0};
+
+static int current_floor = 0;
+
+void floor_init() {
+    if (current_floor < FLOOR_COUNT && current_floor >= 0 && !floors[current_floor].loaded) {
+        floors[current_floor].map = createmap(NUM_ROOMS, MAX_ROOM_X, MIN_ROOM_X, MAX_ROOM_Y, MIN_ROOM_Y, BOARD_Y, BOARD_X);
+        floors[current_floor].enemy_list = list_create();
+        floors[current_floor].loaded = 1;
+        floors[current_floor].down_y = floors[current_floor].down_x = 0;
+        floors[current_floor].up_y = floors[current_floor].up_x = 0;
+    }
+}
+
+void floor_goto(int f) {
+    if (current_floor < FLOOR_COUNT && current_floor >= 0 && !floors[current_floor].loaded) {
+        current_floor = f;
+        floor_init();
+        enemy_set(floors[current_floor].enemy_list);
+        map_load(floots[current_floor].map, BOARD_X, BOARD_Y);
+    }
+}
+
+int floor_get() {
+    return current_floor;
+}
---
+++ floor.h
@@ -0,0 +1,22 @@
+#ifndef FLOOR_H
+#define FLOOR_H
+
+    #define FLOOR_COUNT 10
+    
+    #include "list.h"
+
+    typedef struct floor {
+        int loaded;
+        list_t enemy_list;
+        map_t map;
+        int down_y;
+        int down_x;
+        int up_y;
+        int up_x;
+    } floor_t;
+
+    void floor_init();
+    void floor_goto(int f);
+    int floor_get();
+
+#endif
