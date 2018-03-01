9710c00858c7daea95b0316b10bd5680f9db85f7
phale01 <phale01@saintmarys.edu>
2018-02-28 22:52:06 -0500

    missing import files

---
+++ gui.h
@@ -0,0 +1,15 @@
+#ifndef GUI_H
+#define GUI_H
+	#include <ncurses.h>
+	#include <panel.h>
+	#include <string.h>
+	#include <sys/ioctl.h>
+	#include <stdio.h>
+	#include <unistd.h>
+
+	void init_wins(WINDOW **wins, struct winsize w);
+	void win_show(WINDOW *win, char *label, int label_color);
+	void print_in_window(WINDOW *win, int starty, int startx, int width, char *string, chtype color, bool mid);
+
+
+#endif
