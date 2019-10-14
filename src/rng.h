eca3b736252d625ffe373f86b4c86c9dba5dc042
Apoorv Agrawal <apoorv.agarwal.aga@gmail.com>
2019-10-14 22:58:53 +0530

    Added rng files

---
+++ src/rng.h
@@ -0,0 +1,8 @@
+#ifndef RNG_H
+#define RNG_H
+
+//Constants for probabilities
+
+int rng_rand(int max); //returns a random number in the range [0, max]
+int rng_roll(int droprate); //returns 1 with probability 1/droprate, else 0
+#endif
