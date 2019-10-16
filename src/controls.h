0c53406fb3a443118cf93bddbf479c9ec9d5c28a
Luis Galocha <galdomluis@gmail.com>
2019-10-16 11:46:34 +0200

    created controls.h with the definition of keys

---
+++ src/controls.h
@@ -0,0 +1,24 @@
+// movement keys define
+
+
+//Normal movements
+const int MOVE_W = 'j';
+const int MOVE_W_BABBY = '0x102';
+const int MOVE_A = 'h';
+const int MOVE_A_BABBY = '0x104';
+const int MOVE_S = 'k';
+const int MOVE_S_BABBY = '0x103';
+const int MOVE_D = 'l';
+const int MOVE_D_BABBY = '0x105';
+const int MOVE_D = 'l';
+
+//diagonal movement
+const int MOVE_A_W = 'b';
+const int MOVE_D_W = 'n';
+const int MOVE_A_S = 'y';
+const int MOVE_W_S = 'u';
+
+//actions
+const int KEY_< = '<';
+const int KEY_Q = 'q';
+const int KEY_E = 'l';
