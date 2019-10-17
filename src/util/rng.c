9710a08032838493569e9e06e53b1b37e2fcccf6
John Westhoff <JohnJWesthoff@gmail.com>
2019-10-17 18:09:53 -0400

    Calmed down linter

--- src/util/rng.c
+++ src/util/rng.c
@@ -1,13 +1,15 @@
 #include "rng.h"
 #include <stdlib.h>
 
-int rng_rand(int max){
-    int limit = RAND_MAX - (RAND_MAX % (max + 1)),r;
-    while((r = rand()) >= limit);
+int rng_rand(int max)
+{
+    int limit = RAND_MAX - (RAND_MAX % (max + 1)), r;
+    while ((r = rand()) >= limit) ;
     return r % (max + 1);
 }
 
-int rng_roll(int droprate){
+int rng_roll(int droprate)
+{
     int r = rng_rand(droprate);
-    return (r==0)?1:0 ;
+    return (r == 0) ? 1 : 0;
 }
