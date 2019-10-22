ffd2327e215348d6ebd60b98c437462afdcd3d96
Luis Galocha <galdomluis@gmail.com>
2019-10-22 14:49:11 +0200

    improved names of constants

--- src/controls.h
+++ src/controls.h
@@ -2,23 +2,23 @@
 
 
 //Normal movements
-const int MOVE_W = 'j';
-const int MOVE_W_BABBY = '0x102';
-const int MOVE_A = 'h';
-const int MOVE_A_BABBY = '0x104';
+const int MOVE_N = 'j';
+const int MOVE_N_BABBY = '0x102';
+const int MOVE_W = 'h';
+const int MOVE_W_BABBY = '0x104';
 const int MOVE_S = 'k';
 const int MOVE_S_BABBY = '0x103';
-const int MOVE_D = 'l';
-const int MOVE_D_BABBY = '0x105';
-const int MOVE_D = 'l';
+const int MOVE_E = 'l';
+const int MOVE_E_BABBY = '0x105';
 
 //diagonal movement
-const int MOVE_A_W = 'b';
-const int MOVE_D_W = 'n';
-const int MOVE_A_S = 'y';
-const int MOVE_W_S = 'u';
+const int MOVE_NW = 'b';
+const int MOVE_NE = 'n';
+const int MOVE_SW = 'y';
+const int MOVE_SE = 'u';
 
 //actions
-const int KEY_< = '<';
+const int KEY_CLIMB_UP = '<';
+const int KEY_CLIMB_DOWN = '>';
 const int KEY_Q = 'q';
 const int KEY_E = 'l';
